5173b73e6285d7c356c0e4ec38989a893f9ab90f
Vishal <vishal.sakaria@wipro.com>
2019-12-02 11:57:35 +0000

    ADD req.query & res.json

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  const { name, age, cool } = req.query;
+  const vishal = { name, age, cool };
+  // res.send("Hey! It works!");
+
+  res.json(vishal);
 });
 
 module.exports = router;
